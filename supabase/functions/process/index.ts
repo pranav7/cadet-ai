e139dfc1c2e9aea412ce6ad39f9828aa76ca9f76
Pranav <pranav@june.so>
2025-02-10 11:26:48 +0000

    Process function deno setup

---
+++ supabase/functions/process/index.ts
@@ -0,0 +1,34 @@
+import { createClient } from "@supabase/supabase-js";
+
+// These are automatically injected
+const supabaseUrl = Deno.env.get("SUPABASE_URL")!;
+const supabaseAnonKey = Deno.env.get("SUPABASE_ANON_KEY")!;
+
+Deno.serve((req: Request): Response => {
+  const authorization = req.headers.get("Authorization");
+
+  if (!authorization) {
+    return new Response(
+      JSON.stringify({ error: `No authorization header passed` }),
+      {
+        status: 500,
+        headers: { "Content-Type": "application/json" },
+      },
+    );
+  }
+
+  const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+    global: {
+      headers: {
+        authorization,
+      },
+    },
+    auth: {
+      persistSession: false,
+    },
+  });
+
+  return new Response(JSON.stringify({ success: true }), {
+    headers: { "Content-Type": "application/json" },
+  });
+});
--- supabase/migrations/20250209142059_documents.sql
+++ supabase/migrations/20250209142059_documents.sql
@@ -26,56 +26,70 @@ create index on private.document_chunks using hnsw (embedding vector_ip_ops);
 alter table private.documents enable row level security;
 alter table private.document_chunks enable row level security;
 
-create policy "Users can query their team's documents"
+create policy "Users can query their account's documents"
 on private.documents for select to authenticated using (
-  auth.uid() in (
-    select created_by
-    from private.documents
-    where account_id = private.documents.account_id
-  )
+  (select account_id from auth.users where id = auth.uid()) = account_id
 );
 
-create policy "Users can query their team's document chunks"
+create policy "Users can query their account's document chunks"
 on private.document_chunks for select to authenticated using (
-  auth.uid() in (
-    select created_by
-    from private.documents
-    where account_id = private.document_chunks.account_id
-  )
+  (select account_id from auth.users where id = auth.uid()) = account_id
 );
 
 create policy "Users can insert documents"
 on private.documents for insert to authenticated with check (
-  auth.uid() in (
-    select created_by
-    from private.documents
-    where account_id = private.documents.account_id
-  )
+  (select account_id from auth.users where id = auth.uid()) = account_id
 );
 
 create policy "Users can insert document chunks"
 on private.document_chunks for insert to authenticated with check (
-  auth.uid() in (
-    select created_by
-    from private.documents
-    where account_id = private.document_chunks.account_id
-  )
+  (select account_id from auth.users where id = auth.uid()) = account_id
 );
 
 create policy "Users can update their documents"
 on private.documents for update to authenticated using (
-  auth.uid() in (
-    select created_by
-    from private.documents
-    where account_id = private.documents.account_id
-  )
+  (select account_id from auth.users where id = auth.uid()) = account_id
 );
 
 create policy "Users can update their document chunks"
 on private.document_chunks for update to authenticated using (
-  auth.uid() in (
-    select created_by
-    from private.documents
-    where account_id = private.document_chunks.account_id
-  )
+  (select account_id from auth.users where id = auth.uid()) = account_id
 );
+
+create function supabase_url()
+returns text
+language plpgsql
+security definer
+as $$
+declare
+  secret_value text;
+begin
+  select decrypted_secret into secret_value from vault.decrypted_secrets where name = 'supabase_url';
+  return secret_value;
+end;
+$$;
+
+create function private.handle_document_insert()
+returns trigger
+language plpgsql
+as $$
+begin
+  perform net.http_post(
+    url := supabase_url() || '/functions/v1/process',
+    headers := jsonb_build_object(
+      'Content-Type', 'application/json',
+      'Authorization', current_setting('request.headers')::json->>'authorization'
+    ),
+    body := jsonb_build_object(
+      'document_id', new.id
+    )
+  );
+
+  return new;
+end;
+$$;
+
+create trigger on_document_insert
+  after insert on private.documents
+  for each row
+  execute procedure private.handle_document_insert();
